85f97237d5f69862135caad9ed93bc689147c04e
Conite002 <conite.gbodogbe@imsp-uac.org>
2024-02-25 16:18:36 +0100

    Update Firebase configuration using environment variables

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.post('/', async (req, res) => {
         console.log('req : ', xfdfData);
 
         const firebaseConfig = {
-            apiKey: "AIzaSyAJLiKBxvNaV82B5MYnaggZJaOUKBQo9Ts",
-            authDomain: "db-dce.firebaseapp.com",
-            projectId: "db-dce",
-            storageBucket: "db-dce.appspot.com",
-            messagingSenderId: "801498778018",
-            appId: "1:801498778018:web:a7e801119a2b7189bd4afb"
+            apiKey: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.FIREBASE_APP_ID
         };
           
         const appFirebase = initializeApp(firebaseConfig);
